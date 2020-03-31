6b23b9c1e90d76658eb23d6a1bdba2f2a8e913c4
Gh0u1L5 <Gh0u1L5@outlook.com>
2020-03-31 13:35:01 +0800

    Fix the wrong way to use Promise().

--- src/definition.ts
+++ src/definition.ts
@@ -10,66 +10,67 @@ export class SmaliDefinitionProvider implements vscode.DefinitionProvider {
         document: vscode.TextDocument,
         position: vscode.Position,
         token: vscode.CancellationToken
-    ): vscode.Location | vscode.Location[] | vscode.LocationLink[] {
-        let locations = new Array<vscode.Location>();
+    ): Thenable<vscode.Definition | vscode.DefinitionLink[]> {
+        return new Promise((resolve) => {
+            let locations = new Array<vscode.Location>();
 
-        let type = smali_language.AsType(document, position);
-        if (type) {
-            let classfile_name = type2fspath(type.spot);
-            for (const jclass of jclasses) {
-                if (jclass[0].path.endsWith(classfile_name)) {
-                    locations.push(new vscode.Location(jclass[0], new vscode.Position(0, 0)));
-                }
-            }
-        }
-
-        let field = smali_language.AsField(document, position);
-        if (field) {
-            let classfile_name = type2fspath(field.owner);
-            for (const jclass of jclasses) {
-                if (jclass[0].path.endsWith(classfile_name)) {
-                    if (!jclass[1]) {
-                        let doc = readFileSync(jclass[0].fsPath).toString();
-                        jclass[1] = smali_language.ParseSmali(doc);
-                    }
-                    for (const _field of jclass[1].Fields) {
-                        if (field.field.equal(_field)) {
-                            locations.push(new vscode.Location(jclass[0], _field.Range));
-                        }
+            let type = smali_language.AsType(document, position);
+            if (type) {
+                let classfile_name = type2fspath(type.spot);
+                for (const jclass of jclasses) {
+                    if (jclass[0].path.endsWith(classfile_name)) {
+                        locations.push(new vscode.Location(jclass[0], new vscode.Position(0, 0)));
                     }
                 }
             }
-        }
 
-        let method = smali_language.AsMethod(document, position);
-        if (method) {
-            let classfile_name = type2fspath(method.owner);
-            for (const jclass of jclasses) {
-                if (jclass[0].path.endsWith(classfile_name)) {
-                    if (!jclass[1]) {
-                        let doc = readFileSync(jclass[0].fsPath).toString();
-                        jclass[1] = smali_language.ParseSmali(doc);
-                    }
-                    if (method.spot instanceof smali_language.Constructor) {
-                        for (const _cotr of jclass[1].Constructors) {
-                            if (method.spot.equal(_cotr)) {
-                                locations.push(new vscode.Location(jclass[0], _cotr.Range));
-                            }
+            let field = smali_language.AsField(document, position);
+            if (field) {
+                let classfile_name = type2fspath(field.owner);
+                for (const jclass of jclasses) {
+                    if (jclass[0].path.endsWith(classfile_name)) {
+                        if (!jclass[1]) {
+                            let doc = readFileSync(jclass[0].fsPath).toString();
+                            jclass[1] = smali_language.ParseSmali(doc);
                         }
-                    }
-                    if (method.spot instanceof smali_language.Method) {
-                        for (const _method of jclass[1].Methods) {
-                            if (method.spot.equal(_method)) {
-                                locations.push(new vscode.Location(jclass[0], _method.Range));
+                        for (const _field of jclass[1].Fields) {
+                            if (field.field.equal(_field)) {
+                                locations.push(new vscode.Location(jclass[0], _field.Range));
                             }
                         }
                     }
                 }
             }
-        }
 
+            let method = smali_language.AsMethod(document, position);
+            if (method) {
+                let classfile_name = type2fspath(method.owner);
+                for (const jclass of jclasses) {
+                    if (jclass[0].path.endsWith(classfile_name)) {
+                        if (!jclass[1]) {
+                            let doc = readFileSync(jclass[0].fsPath).toString();
+                            jclass[1] = smali_language.ParseSmali(doc);
+                        }
+                        if (method.spot instanceof smali_language.Constructor) {
+                            for (const _cotr of jclass[1].Constructors) {
+                                if (method.spot.equal(_cotr)) {
+                                    locations.push(new vscode.Location(jclass[0], _cotr.Range));
+                                }
+                            }
+                        }
+                        if (method.spot instanceof smali_language.Method) {
+                            for (const _method of jclass[1].Methods) {
+                                if (method.spot.equal(_method)) {
+                                    locations.push(new vscode.Location(jclass[0], _method.Range));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
 
-        return locations;
+            resolve(locations);
+        });
     }
 }
 
--- src/symbol.ts
+++ src/symbol.ts
@@ -3,41 +3,44 @@ import * as extension from './extension';
 
 export class SmaliDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
     public provideDocumentSymbols(
-        document: vscode.TextDocument, token: vscode.CancellationToken):
-        Thenable<vscode.SymbolInformation[]> {
-        let jclass = extension.ParseTextDocument(document);
-        if (jclass === null) {
-            return null;
-        }
-        let symbols = new Array<vscode.SymbolInformation>();
+        document: vscode.TextDocument,
+        token: vscode.CancellationToken
+    ): Thenable<vscode.SymbolInformation[]> {
+        return new Promise<vscode.SymbolInformation[]>((resolve) => {
+            let jclass = extension.ParseTextDocument(document);
+            if (jclass === null) {
+                return null;
+            }
+            let symbols = new Array<vscode.SymbolInformation>();
 
-        for (const field of jclass.Fields) {
-            symbols.push(new vscode.SymbolInformation(
-                field.Name,
-                vscode.SymbolKind.Field,
-                field.Modifiers.join(' ') +' '+ field.Type,
-                new vscode.Location(document.uri, field.Range)
-            ));
-        }
+            for (const field of jclass.Fields) {
+                symbols.push(new vscode.SymbolInformation(
+                    field.Name,
+                    vscode.SymbolKind.Field,
+                    field.Modifiers.join(' ') +' '+ field.Type,
+                    new vscode.Location(document.uri, field.Range)
+                ));
+            }
 
-        for (const ctor of jclass.Constructors) {
-            symbols.push(new vscode.SymbolInformation(
-                `Constructors(${ctor.Parameters.join(' , ')})`,
-                vscode.SymbolKind.Constructor,
-                ctor.Name,
-                new vscode.Location(document.uri, ctor.Range)
-            ));
-        }
+            for (const ctor of jclass.Constructors) {
+                symbols.push(new vscode.SymbolInformation(
+                    `Constructors(${ctor.Parameters.join(' , ')})`,
+                    vscode.SymbolKind.Constructor,
+                    ctor.Name,
+                    new vscode.Location(document.uri, ctor.Range)
+                ));
+            }
 
-        for (const method of jclass.Methods) {
-            symbols.push(new vscode.SymbolInformation(
-                `${method.Name}(${method.Parameters.join(' , ')})`,
-                vscode.SymbolKind.Method,
-                method.ReturnType.Readable,
-                new vscode.Location(document.uri, method.Range)
-            ));
-        }
+            for (const method of jclass.Methods) {
+                symbols.push(new vscode.SymbolInformation(
+                    `${method.Name}(${method.Parameters.join(' , ')})`,
+                    vscode.SymbolKind.Method,
+                    method.ReturnType.Readable,
+                    new vscode.Location(document.uri, method.Range)
+                ));
+            }
 
-        return Promise.resolve(symbols);
+            resolve(symbols);
+        });
     }
 }
