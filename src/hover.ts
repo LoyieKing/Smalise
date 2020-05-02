284ae1109fc64d8af6843028c053db96631cc6ad
Gh0u1L5 <Gh0u1L5@outlook.com>
2020-05-02 23:33:10 +0800

    Use code block to limit the variable scope and prevent unexpected bugs.

--- src/hover.ts
+++ src/hover.ts
@@ -8,50 +8,55 @@ export class SmaliHoverProvider implements vscode.HoverProvider {
         token: vscode.CancellationToken
     ): vscode.Hover {
         let line = document.lineAt(position.line);
-
-        let str = findString(document, position);
-        if (str) {
-            return new vscode.Hover({ language: 'java', value: unescape(str.text.replace(/\\u/g, '%u')) }, str.range);
-        }
-
-        let type = findType(document, position);
-        if (type) {
-            return new vscode.Hover({ language: 'java', value: type.toString() }, type.range);
-        }
-
-        let myfield = findFieldDefinition(document, position);
-        if (myfield) {
-            return new vscode.Hover({ language: 'java', value: myfield.toString() }, line.range);
-        }
-
-        let mymethod = findMethodDefinition(document, position);
-        if (mymethod) {
-            return new vscode.Hover({ language: 'java', value: mymethod.toString() }, line.range);
-        }
-
-        let { owner: fowner, field } = findFieldReference(document, position);
-        if (fowner && field) {
-            return new vscode.Hover({
-                language: 'java',
-                value: field.toString(fowner.toString() + '.' + field.name.text)
-            }, field.range);
-        }
-
-        let { owner: mowner, method } = findMethodReference(document, position);
-        if (mowner && method) {
-            if (method.isConstructor) {
-                return new vscode.Hover({
-                    language: 'java',
-                    value: method.toString('new ' + mowner.toString())
-                }, method.range);
-            } else {
-                return new vscode.Hover({
-                    language: 'java',
-                    value: method.toString(mowner.toString() + '.' + method.name.text)
-                }, method.range);
+        {
+            let str = findString(document, position);
+            if (str) {
+                return new vscode.Hover({ language: 'java', value: unescape(str.text.replace(/\\u/g, '%u')) }, str.range);
             }
         }
-
+        {
+            let type = findType(document, position);
+            if (type) {
+                return new vscode.Hover({ language: 'java', value: type.toString() }, type.range);
+            }
+        }
+        {
+            let myfield = findFieldDefinition(document, position);
+            if (myfield) {
+                return new vscode.Hover({ language: 'java', value: myfield.toString() }, line.range);
+            }
+        }
+        {
+            let mymethod = findMethodDefinition(document, position);
+            if (mymethod) {
+                return new vscode.Hover({ language: 'java', value: mymethod.toString() }, line.range);
+            }
+        }
+        {
+            let { owner, field } = findFieldReference(document, position);
+            if (owner && field) {
+                return new vscode.Hover({
+                    language: 'java',
+                    value: field.toString(owner.toString() + '.' + field.name.text)
+                }, field.range);
+            }
+        }
+        {
+            let { owner, method } = findMethodReference(document, position);
+            if (owner && method) {
+                if (method.isConstructor) {
+                    return new vscode.Hover({
+                        language: 'java',
+                        value: method.toString('new ' + owner.toString())
+                    }, method.range);
+                } else {
+                    return new vscode.Hover({
+                        language: 'java',
+                        value: method.toString(owner.toString() + '.' + method.name.text)
+                    }, method.range);
+                }
+            }
+        }
         return null;
     }
 }
--- src/reference.ts
+++ src/reference.ts
@@ -10,49 +10,59 @@ export class SmaliReferenceProvider implements vscode.ReferenceProvider {
         context: vscode.ReferenceContext,
         token: vscode.CancellationToken
     ): Promise<vscode.Location[]> {
-        let owner = findClassName(document);
-
-        let type = findType(document, position);
-        if (type && type.identifier) {
-            let locations = await extension.searchSymbolReference([
-                type.identifier,
-                '"' + type.identifier.slice(0, -1) + '"',
-            ]);
-            return [].concat(...locations);
-        }
-
-        let myfield = findFieldDefinition(document, position);
-        if (owner && myfield) {
-            let locations = await extension.searchSymbolReference([owner + '->' + myfield.getIdentifier()]);
-            return locations[0];
-        }
-
-        let mymethod = findMethodDefinition(document, position);
-        if (owner && mymethod) {
-            let subclasses: string[] = new Array();
-            let roots = await extension.searchRootClassIdsForMethod(owner, mymethod);
-            for (const root of roots) {
-                subclasses = subclasses.concat(root, ...await extension.searchSmaliSubclassIds(root));
+        {
+            let type = findType(document, position);
+            if (type && type.identifier) {
+                let locations = await extension.searchSymbolReference([
+                    type.identifier,
+                    '"' + type.identifier.slice(0, -1) + '"',
+                ]);
+                return [].concat(...locations);
             }
-            let references = subclasses.map(id => id + '->' + mymethod.getIdentifier());
-            return [].concat(...await extension.searchSymbolReference(references));
         }
-
-        let { owner: fowner, field } = findFieldReference(document, position);
-        if (fowner && field) {
-            let locations = await extension.searchSymbolReference([fowner.identifier + '->' + field.getIdentifier()]);
-            return locations[0];
-        }
-
-        let { owner: mowner, method } = findMethodReference(document, position);
-        if (mowner && method) {
-            let subclasses: string[] = new Array();
-            let roots = await extension.searchRootClassIdsForMethod(mowner.identifier, method);
-            for (const root of roots) {
-                subclasses = subclasses.concat(root, ...await extension.searchSmaliSubclassIds(root));
+        {
+            let myfield = findFieldDefinition(document, position);
+            if (myfield) {
+                let owner = findClassName(document);
+                if (owner) {
+                    let locations = await extension.searchSymbolReference([owner + '->' + myfield.getIdentifier()]);
+                    return locations[0];
+                }
             }
-            let references = subclasses.map(id => id + '->' + method.getIdentifier());
-            return [].concat(...await extension.searchSymbolReference(references));
+        }
+        {
+            let mymethod = findMethodDefinition(document, position);
+            if (mymethod) {
+                let owner = findClassName(document);
+                if (owner) {
+                    let subclasses: string[] = new Array();
+                    let roots = await extension.searchRootClassIdsForMethod(owner, mymethod);
+                    for (const root of roots) {
+                        subclasses = subclasses.concat(root, ...await extension.searchSmaliSubclassIds(root));
+                    }
+                    let references = subclasses.map(id => id + '->' + mymethod.getIdentifier());
+                    return [].concat(...await extension.searchSymbolReference(references));
+                }
+            }
+        }
+        {
+            let { owner, field } = findFieldReference(document, position);
+            if (owner && field) {
+                let locations = await extension.searchSymbolReference([owner.identifier + '->' + field.getIdentifier()]);
+                return locations[0];
+            }
+        }
+        {
+            let { owner, method } = findMethodReference(document, position);
+            if (owner && method) {
+                let subclasses: string[] = new Array();
+                let roots = await extension.searchRootClassIdsForMethod(owner.identifier, method);
+                for (const root of roots) {
+                    subclasses = subclasses.concat(root, ...await extension.searchSmaliSubclassIds(root));
+                }
+                let references = subclasses.map(id => id + '->' + method.getIdentifier());
+                return [].concat(...await extension.searchSymbolReference(references));
+            }
         }
     }
 }
